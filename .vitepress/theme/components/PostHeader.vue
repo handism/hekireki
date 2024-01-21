ca8eb517fdefa8542edefa43c8b350ff6ba8f452
handism <andy2roidy@gmail.com>
2024-01-21 18:51:15 +0900

    hekireki Ver1.0リリース

--- .vitepress/theme/components/PostHeader.vue
+++ .vitepress/theme/components/PostHeader.vue
@@ -11,7 +11,7 @@ const { base } = withBase()
     <time v-if="frontmatter.tags" class="last-updated">{{ new Date(page.lastUpdated).toLocaleDateString("ja-JP", {year: "numeric",month: "2-digit", day: "2-digit"}) }}</time>
     <h1 v-if="frontmatter.tags" class="post-title">{{ frontmatter.title }}</h1>   
     <span v-for="tag in frontmatter.tags" class="tags">
-      <a :href="`${base}/tag/${tag.toLowerCase().replace(' ', '-')}`" class="tag">{{ "# " + tag }}</a>
+      <a :href="`${base()}/tag/${tag.toLowerCase().replace(' ', '-')}`" class="tag">{{ "# " + tag }}</a>
     </span>
     <img v-if="frontmatter.image" :src="frontmatter.image" class="thumbnail" alt="ブログのサムネイル画像">
   </div>
